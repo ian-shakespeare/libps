5cd71c25e78d5ec5c835a555757dc3a31697d4a3
Ian Shakespeare <ian@shakespeare.dev>
2024-12-09 23:13:05 -0700

    switch to go, start scanner

---
+++ pkg/utils/array.go
@@ -0,0 +1,20 @@
+package utils
+
+// Returns the index of the first element that is true on the condition.
+// Otherwise, returns -1.
+func Some[T any](arr []T, cond func(T) bool) int {
+	for i := 0; i < len(arr); i++ {
+		if cond(arr[i]) {
+			return i
+		}
+	}
+	return -1
+}
+
+// Returns true if the array contains the given value.
+func Contains[T comparable](arr []T, value T) bool {
+	index := Some(arr, func(elem T) bool {
+		return elem == value
+	})
+	return index > -1
+}
--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod operator;
-pub mod parse;
-pub mod stack;
-pub mod token;
-
-pub use crate::parse::parse;
-pub use crate::token::tokenize;
